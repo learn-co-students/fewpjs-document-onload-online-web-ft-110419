0bac0c4ad9c77e1d3367f59ee5025138c92b0ce9
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-02-19 21:20:06 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("text").innerHTML = "This is really cool!";
   })
+
+  /* alt 
+  function changeText(){
+      document.getElementById("text").innerHTML = "This is really cool!";
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+    changeText();
+  })
