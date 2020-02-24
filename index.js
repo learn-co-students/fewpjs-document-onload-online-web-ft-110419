bfad091cc6d3093e4a66540b886431de7fc6f732
cejimenez0123 <christianjimenez0123@gmail.com>
2020-02-24 10:51:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+function click(){
+    var ptag = document.getElementById("text")
+ptag.textContent = ptag.textContent.replace("JavaScript is so cool. It lets me add text to my page programmatically.","This is really cool!")
+}
+document.addEventListener("DOMContentLoaded", function(){
+    click()
+});
